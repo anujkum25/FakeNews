8c568c7ed57d48a225e283292a99cf3026d660e8
Anuj Kumar <anuj.kumar05@sap.com>
2020-05-29 16:53:36 +0530

    improved app and evaluation

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Output, State, Input
+import dash_table
+import pandas as pd
+from evaluation import evaluate_model
+
+'''
+Many stuff in this file is hard-coded, to be chnaged later
+'''
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+df= pd.read_csv(".\Data\confusion_matrix.csv")
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
@@ -28,28 +38,52 @@ app.layout = html.Div([
             
             html.Div(id='textarea1_op', style={'whiteSpace': 'pre-line'}),
             
-            html.Button('Submit', id='submit-val', n_clicks=0)
+            html.Button('Submit', id='submit-val', type='submit')
             ]),
             
             dcc.Tab(label='ResultPage', children=[
-                
+                html.Div(id='textarea-output', style={'whiteSpace': 'pre-line'})
             ]),
             
             dcc.Tab(label='Visualization', children=[
+                
+                html.Br(),
+                
+                dcc.Markdown('''
+                             Comparison between Roberta_most_accurate and Roberta_standard
+                             '''),
+            
                 dcc.Graph(
                 figure={
                     'data': [
-                        {'x': ["Accuracy", "Precision", "Recall"], 'y': [0.99, 0.95, .84],
-                            'type': 'bar', 'name': 'Roberta'},
-                        {'x': ["Accuracy", "Precision", "Recall"], 'y': [0.98,0.9, 0.88],
-                         'type': 'bar', 'name': 'Bert'},
+                        {'x': ["Accuracy", "Precision", "Recall"], 'y': [0.99, 0.95, .88],
+                            'type': 'bar', 'name': 'Roberta_most_accurate'},
+                        {'x': ["Accuracy", "Precision", "Recall"], 'y': [0.98,0.9, 0.84],
+                         'type': 'bar', 'name': 'Roberta_standard'},
                     ]
                 }
-            )
+            ),
+                dcc.Markdown('''
+                             Confusion Matrix for Roberta_standard
+                             '''),
+            
+                dash_table.DataTable(
+                    id='table',
+                    columns=[{"name": i, "id": i} for i in df.columns],
+                    data=df.to_dict('records'),
+                    )
+
 
             ])
     ])
 ])
+
+@app.callback(
+    Output('textarea-output', 'children'),
+    [Input('textarea1', 'value')]
+)
+def update_output(value):
+    return("input text \n",value, "\n result processing..will be available shortly \n", "Result:",evaluate_model([value]))
 
 
 
--- evaluation.py
+++ evaluation.py
@@ -2,13 +2,15 @@ import os
 from simpletransformers.classification import ClassificationModel
 from source_selection import saved_model_path
 
-def evaluate_model():
+def evaluate_model(input_text):
     '''
     after model is trained, we use trained model to do predictions
+    input_text should be a list
     '''
     # within saved_model_path, there shall be multiple UUID for each model, one
-    # needs to be picked and passed here.
-    
+    # needs to be picked and passed here. change directory path to that subfolder.
+    os.chdir("C:\\Users\\i345144\\OneDrive - SAP SE\\Documents\\MSRUS\\Group_Project\\pytoruch\\")
+
 
     # TODO:create a function for model_args in models.py and then call that here. 
        
@@ -24,7 +26,10 @@ def evaluate_model():
         }
 
     model=ClassificationModel('roberta',"outputs\\", use_cuda=False, args=model_args)
-    predictions, raw_outputs = model.predict(['Coronavirus was first found in China'])
+    predictions, raw_outputs = model.predict(input_text)
 
-    return(predictions)
+    if predictions[0]==[0]:
+        return ("Real")
+    else:
+        return ("Fake")
 
